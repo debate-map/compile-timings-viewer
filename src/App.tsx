4ba3201d609ced0ff249a50056a85a49706495e2
rishadbaniya <rbdroid01@gmail.com>
2024-10-10 20:55:31 +0545

    feat: add custom tick rendering for bottom axis

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,61 @@
-import { ResponsiveBar } from '@nivo/bar'
+import { BarDatum, ResponsiveBar } from '@nivo/bar'
+import {useState, useEffect} from 'react';
+import { BuildMetadata, fetchBuildMetadata, fetchTrackerData } from './dataProvider';
+
+type BarGraphBuildMetadata = {
+    compile_date: number;
+    time_taken: number;
+}
 
 const App = () => {
+    const [buildMetadata, setBuildMetadata] = useState<BarGraphBuildMetadata[]>([]);
+
+    useEffect(() => {
+        fetchTrackerData().then((trackerData) => {
+            Promise.all(trackerData.map(t => fetchBuildMetadata(t)))
+                .then(metadataArray => {
+                   setBuildMetadata(metadataArray.map((metadata: BuildMetadata) => {
+                        return {
+                            compile_date: metadata.b,
+                            time_taken: metadata.t
+                        }
+                    }))
+                });
+        });
+    }, []);
+
     return <>
-    <div style={{ height: '96vh', width: '99vw' }}>
-      <BarGraph />
-    </div>
+        <div style={{ height: '96vh', width: '99vw' }}>
+          <BarGraph data={buildMetadata}/>
+        </div>
     </>
 }
 
-const data = [
-  {
-    "compile_date": "AD",
-    "time_taken": 184,
-  },
-];
-
-const BarGraph = () => (
+const theme = {
+   axis : {
+    ticks: {
+         text: {
+           fontSize: 18
+         }
+       },
+    legend: {
+         text: {
+           fontSize: 24
+         }
+       }
+    }
+};
+const BarGraph = <T extends BarDatum>({data} : {data: T[]}) => (
+    //textStyle?: Partial<React.CSSProperties>
     <ResponsiveBar
+        theme={theme}
         data={data}
+
         keys={[
             'time_taken'
         ]}
         indexBy="compile_date"
-        margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
+        margin={{ top: 50, right: 50, bottom: 100, left: 100 }}
         padding={0.1}
         valueScale={{ type: 'linear' }}
         indexScale={{ type: 'band', round: true }}
@@ -50,58 +83,61 @@ const BarGraph = () => (
         axisTop={null}
         axisRight={null}
         axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Compile Date',
+            tickSize: 16,
+            legend: 'Compilation Date & Time',
             legendPosition: 'middle',
-            legendOffset: 32,
-            truncateTickAt: 20
+            legendOffset: 90,
+            truncateTickAt: 20,
+            renderTick: (tick) => {
+                return (
+                    <g transform={`translate(${tick.x},${tick.y + 16})`}>
+                        <text
+                            x={0}
+                            y={0}
+                            dy={16}
+                            textAnchor="middle"
+                            style={{
+                                fontSize: 18,
+                                fill: 'black'
+                            }}
+                        >
+                            {new Date(tick.value * 1000).toLocaleDateString('en-CA')}
+                        </text>
+                        <text
+                            x={0}
+                            y={20}
+                            dy={16}
+                            textAnchor="middle"
+                            style={{
+                                fontSize: 18,
+                                fill: 'black'
+                            }}
+                        >
+                            {new Date(tick.value * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                        </text>
+                    </g>
+                )
+            }
         }}
         axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Compile Time',
+            tickSize: 8,
+            tickPadding: 8,
+            legend: 'Time Taken(in seconds)',
             legendPosition: 'middle',
-            legendOffset: -40,
-            truncateTickAt: 0
+            legendOffset: -80,
+            truncateTickAt: 12,
         }}
         labelSkipWidth={12}
         labelSkipHeight={12}
-        labelTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
+        //labelTextColor={{
+            //from: 'color',
+            //modifiers: [
+                //[
+                    //'darker',
+                   //5
+                //]
+            //]
+        //}}
         role="application"
         ariaLabel="Compile Time Graph"
     />
---
+++ src/dataProvider.ts
@@ -0,0 +1,69 @@
+/**
+ * An array of timestamps in the format "YYYYMMDDTHHMMSSZ".
+ */
+export type TrackerData = string[];
+
+/** Represents metadata for a build process. **/
+export type BuildMetadata = {
+  /** Total build time in seconds */
+  t: number;
+
+  /** Version of the Rust compiler used */
+  r: string;
+
+  /** Number of compilation units */
+  u: number;
+
+  /** Build Unix timestamp */
+  b: number;
+};
+
+/** Represents data for individual build units. **/
+export type BuildUnitsData = {
+  /** Name of the compilation unit */
+  u: string;
+
+  /** Build time taken for this unit in seconds */
+  t: number;
+};
+
+export const fetchTrackerData = async (): Promise<TrackerData> => {
+  try {
+      const resp = await fetch('https://debate-map.github.io/compile-timings/timings/tracker.json'); // NOTE: absolute url is used for ease of development now
+      const data = await resp.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching tracker data:', error);
+      throw error;
+    }
+};
+
+/**
+ * Fetches build metadata for a specific timestamp.
+ * @param {string} timestamp - The timestamp in the format "YYYYMMDDTHHMMSSZ".
+ */
+export const fetchBuildMetadata = async (timestamp: string): Promise<BuildMetadata> => {
+  try {
+    const resp = await fetch(`https://debate-map.github.io/compile-timings/timings/build_metadatas/metadata_${timestamp}.json`); // NOTE: absolute url is used for ease of development now
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching build metadata:', error);
+    throw error;
+  }
+};
+
+/**
+ * Fetches build units for a specific timestamp.
+ * @param {string} timestamp - The timestamp in the format "YYYYMMDDTHHMMSSZ".
+ */
+export const fetchBuildUnitsData = async (timestamp : string) : Promise<BuildUnitsData[]> => {
+  try {
+    const resp = await fetch(`https://debate-map.github.io/compile-timings/timings/build_metadatas/units_${timestamp}.json`); // NOTE: absolute url is used for ease of development now
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching build metadata:', error);
+    throw error;
+  }
+}
