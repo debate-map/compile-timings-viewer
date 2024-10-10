74e05214aa3f5982d90d11e3ae7cf9c906c44748
rishadbaniya <rbdroid01@gmail.com>
2024-10-10 21:49:37 +0545

    fix: configure label, legend and tick

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,99 @@ type BarGraphBuildMetadata = {
     time_taken: number;
 }
 
+interface TickProps {
+  x: number;
+  y: number;
+  value: number;
+}
+
+const theme = {
+   axis : {
+    ticks: {
+         text: {
+           fontSize: 18
+         }
+       },
+    legend: {
+         text: {
+           fontSize: 24
+         }
+       }
+    },
+    labels: {
+        text :{
+          fontSize: 18
+       }
+    }
+};
+
+const DateTimeTickComponent = ({ tick }: { tick: TickProps }) => (
+    <g transform={`translate(${tick.x},${tick.y + 16})`}>
+        <text
+            x={0}
+            y={0}
+            dy={16}
+            textAnchor="middle"
+            style={{
+                fontSize: 18,
+                fill: 'black'
+            }}
+        >
+            {new Date(tick.value * 1000).toLocaleDateString('en-CA')}
+        </text>
+        <text
+            x={0}
+            y={20}
+            dy={16}
+            textAnchor="middle"
+            style={{
+                fontSize: 18,
+                fill: 'black'
+            }}
+        >
+            {new Date(tick.value * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+        </text>
+    </g>
+)
+
+const BarGraph = <T extends BarDatum>({data} : {data: T[]}) => (
+    <ResponsiveBar
+        theme={theme}
+        data={data}
+        keys={[ 'time_taken' ]}
+        indexBy="compile_date"
+        margin={{ top: 50, right: 25, bottom: 100, left: 100 }}
+        padding={0.2}
+        valueScale={{ type: 'linear'}}
+        indexScale={{ type: 'band', round: true }}
+        colors={{ scheme: 'paired'}}
+        axisBottom={{
+            tickSize: 16,
+            legend: 'Compilation Date & Time',
+            legendPosition: 'middle',
+            legendOffset: 90,
+            truncateTickAt: 20,
+            renderTick: (tick) => <DateTimeTickComponent tick={tick} />
+        }}
+        axisLeft={{
+            tickSize: 8,
+            tickPadding: 8,
+            legend: 'Time Taken(in seconds)',
+            legendPosition: 'middle',
+            legendOffset: -80,
+            truncateTickAt: 12,
+        }}
+        label={(datum) => {
+            return `${datum.value}s`
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        tooltipLabel={(_) => "Time Taken" }
+        role="application"
+        ariaLabel="Compile Time Graph"
+    />
+)
+
 const App = () => {
     const [buildMetadata, setBuildMetadata] = useState<BarGraphBuildMetadata[]>([]);
 
@@ -31,115 +124,4 @@ const App = () => {
     </>
 }
 
-const theme = {
-   axis : {
-    ticks: {
-         text: {
-           fontSize: 18
-         }
-       },
-    legend: {
-         text: {
-           fontSize: 24
-         }
-       }
-    }
-};
-const BarGraph = <T extends BarDatum>({data} : {data: T[]}) => (
-    //textStyle?: Partial<React.CSSProperties>
-    <ResponsiveBar
-        theme={theme}
-        data={data}
-
-        keys={[
-            'time_taken'
-        ]}
-        indexBy="compile_date"
-        margin={{ top: 50, right: 50, bottom: 100, left: 100 }}
-        padding={0.1}
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={{ scheme: 'nivo' }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 16,
-            legend: 'Compilation Date & Time',
-            legendPosition: 'middle',
-            legendOffset: 90,
-            truncateTickAt: 20,
-            renderTick: (tick) => {
-                return (
-                    <g transform={`translate(${tick.x},${tick.y + 16})`}>
-                        <text
-                            x={0}
-                            y={0}
-                            dy={16}
-                            textAnchor="middle"
-                            style={{
-                                fontSize: 18,
-                                fill: 'black'
-                            }}
-                        >
-                            {new Date(tick.value * 1000).toLocaleDateString('en-CA')}
-                        </text>
-                        <text
-                            x={0}
-                            y={20}
-                            dy={16}
-                            textAnchor="middle"
-                            style={{
-                                fontSize: 18,
-                                fill: 'black'
-                            }}
-                        >
-                            {new Date(tick.value * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
-                        </text>
-                    </g>
-                )
-            }
-        }}
-        axisLeft={{
-            tickSize: 8,
-            tickPadding: 8,
-            legend: 'Time Taken(in seconds)',
-            legendPosition: 'middle',
-            legendOffset: -80,
-            truncateTickAt: 12,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        //labelTextColor={{
-            //from: 'color',
-            //modifiers: [
-                //[
-                    //'darker',
-                   //5
-                //]
-            //]
-        //}}
-        role="application"
-        ariaLabel="Compile Time Graph"
-    />
-)
 export default App
